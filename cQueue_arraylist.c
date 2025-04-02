d791f2955740c933cf4f9188a5df820dd73164da
Kim taegyu <yjh5331ang@naver.com>
2025-04-02 10:21:41 +0900

    실습03(Queue_ArrayList)_수정중중

--- cQueue_arraylist.c
+++ cQueue_arraylist.c
@@ -29,3 +29,85 @@ typedef struct circularQueue {
     int rear;
 }cQueue;
 
+cQueue* createCircularQueue();
+void enqueue(cQueue* cQueue, int data);
+int isFull(cQueue* cQueue);
+void showQueue(cQueue* cQueue);
+int isEmpty(cQueue* cQueue);
+void dequeue(cQueue* cQueue);
+
+cQueue* createCircularQueue() {
+    cQueue* pCQueue = NULL;
+    int i;
+
+    pCQueue = (cQueue*)malloc(sizeof(cQueue));
+    pCQueue->front = 0;
+    pCQueue->rear = 0;
+
+    return pCQueue;
+}
+
+void showQueue(cQueue* cQueue) {
+
+}
+
+void enqueue(cQueue* cQueue, int data) {
+
+}
+
+void dequeue(cQueue* cQueue, int data) {
+
+}
+
+int isFull(cQueue* cQueue) {
+
+}
+//front와 rear가 같을 때 우리는 empty로 정의했기에
+//꽉 찼을 때의 확인법이 front == rear가 아님
+//생각해보기
+
+int isEmpty(cQueue* cQueue) {
+    if (cQueue->rear == cQueue->front) 
+        return TRUE;
+    else 
+        return FALSE;
+}
+
+int main() {
+    cQueue* cQueue;
+    //Circular Queue 초기화
+    cQueue = createCircularQueue();
+
+    printf("front: %d, rear: %d\n", cQueue->front, cQueue->rear);
+
+    printf("enqueue data 10\n");
+    printf("enqueue data 20\n");
+    printf("enqueue data 30\n");
+    enqueue(cQueue, 10);
+    enqueue(cQueue, 20);
+    enqueue(cQueue, 30);
+    showQueue(cQueue);
+    printf("front: %d, rear: %d\n", cQueue->front, cQueue->rear);
+
+    pritnf("dequeue\n");
+    printf("dequeue\n");
+    dequeue(cQueue);
+    dequeue(cQueue);
+    showQueue(cQueue);
+    printf("front: %d, rear: %d\n", cQueue->front, cQueue->rear);
+
+    printf("enqueue data 40\n");
+    enqueue(cQueue, 40);
+    printf("front: %d, rear: %d\n", cQueue->front, cQueue->rear);
+    printf("enqueue data 50\n");
+    enqueue(cQueue, 50);
+    printf("front: %d, rear: %d\n", cQueue->front, cQueue->rear);
+    printf("enqueue data 60\n");
+    enqueue(cQueue, 60);
+    printf("front: %d, rear: %d\n", cQueue->front, cQueue->rear);
+    printf("enqueue data 70\n");
+    enqueue(cQueue, 70);
+    printf("front: %d, rear: %d\n", cQueue->front, cQueue->rear);
+    showQueue(cQueue);
+    return 0;
+}
