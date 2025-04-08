22ab632d7eb2cc69f403cfd47eff8ff4f1204173
Kim taegyu <yjh5331ang@naver.com>
2025-04-08 11:18:33 +0900

    실습03_1_스스로 해보는 Queue(linkedlist)

--- cQueue_linkedlist.c
+++ cQueue_linkedlist.c
@@ -14,105 +14,112 @@ typedef struct Queue {
     Node* rearPtr; 
 }Queue;
 
-Queue* createCircularQueue();
-void enqueue(Queue* cQueue, int data);
-int isEmpty(Queue* cQueue);
-void dequeue(Queue* cQueue);
-void showQueue(Queue* cQueue);
+Queue* createLinkedListQueue();
+void enqueue(Queue* lQueue, int data);
+int isEmpty(Queue* lQueue);
+void dequeue(Queue* lQueue);
+void showQueue(Queue* lQueue);
 
-Queue* createCircularQueue() {
-    Queue* cQueue = NULL;
+Queue* createLinkedListQueue() {
+    Queue* lQueue = NULL;
 
-    cQueue = (Queue*)malloc(sizeof(Queue));
-    cQueue->frontPtr = NULL;
-    cQueue->rearPtr = NULL;
+    lQueue = (Queue*)malloc(sizeof(Queue));
+    lQueue->frontPtr = NULL;
+    lQueue->rearPtr = NULL;
 
-    return cQueue;
+    return lQueue;
 }
 
-int isEmpty(Queue* cQueue) {
-    if (cQueue->frontPtr == NULL && cQueue->rearPtr == NULL) {
+int isEmpty(Queue* lQueue) {
+    if (lQueue->frontPtr == NULL && lQueue->rearPtr == NULL) {
         return TRUE;
     } else {
         return FALSE;
     }
 }
 
-void enqueue(Queue* cQueue, int data) {
+void enqueue(Queue* lQueue, int data) {
     Node* pNewNode = NULL, *pTmpNode = NULL;
 
     pNewNode = (Node*)malloc(sizeof(Node));
+    //pNewNode 자체는 새로 생성한 노드를 가리킴
     pNewNode->data = data;
     pNewNode->nextNode = NULL;
     printf("enqueue data %d\n", data);
 
-    if (isEmpty(cQueue)) {
-        cQueue->frontPtr = cQueue->rearPtr = pNewNode;
+    if (isEmpty(lQueue)) {
+        lQueue->frontPtr = lQueue->rearPtr = pNewNode;
     } else {
-        pNewNode->nextNode = cQueue->rearPtr;
-        cQueue->rearPtr = pNewNode;
+        pNewNode->nextNode = lQueue->rearPtr;
+        lQueue->rearPtr = pNewNode;
     }
     /*
-    pNewNode->nextNode = cQueue->rearPtr;
-    cQueue->rearPtr = &(pNewNode);
+    pNewNode->nextNode = lQueue->rearPtr;
+    lQueue->rearPtr = &(pNewNode);
     pTmpNode 사용을 안해도 되지않나
     */
 }
 
-void dequeue(Queue* cQueue) {
-    Node* pTmpNode = cQueue->rearPtr;
+void dequeue(Queue* lQueue) {
+    Node* pTmpNode = lQueue->rearPtr;
 
-    if (isEmpty(cQueue)) {
+    if (isEmpty(lQueue)) {
         printf("dequeue error: Empty List\n");
         return;
-    } else if (cQueue->frontPtr == cQueue->rearPtr) {
-        pTmpNode = cQueue->rearPtr;
+    } else if (lQueue->frontPtr == lQueue->rearPtr) {
+        pTmpNode = lQueue->rearPtr;
         free(pTmpNode);
 
-        cQueue->frontPtr = NULL;
-        cQueue->rearPtr = NULL;
+        lQueue->frontPtr = NULL;
+        lQueue->rearPtr = NULL;
+        printf("dequeue\n");
+        return;
     }
     //front의 바로 앞 까지 접근
     while (pTmpNode->nextNode->nextNode != NULL) {
         pTmpNode = pTmpNode->nextNode;
     }
     printf("dequeue\n");
-    cQueue->frontPtr = pTmpNode;
+    lQueue->frontPtr = pTmpNode;
     
     free(pTmpNode->nextNode);
     pTmpNode->nextNode = NULL;
 }
 
-void showQueue(Queue* cQueue) {
+void showQueue(Queue* lQueue) {
     Node* pTmpNode = NULL;
 
-    if (isEmpty(cQueue)) {
-        printf("Queue is empty!\n");
-    } else {
-        pTmpNode = cQueue->rearPtr;
+    if (isEmpty(lQueue)) {
         printf("========show queue========\n");
+        printf("Queue is empty!\n");
+        printf("==========================\n");
+    } else {
+        pTmpNode = lQueue->rearPtr;
+        printf("========show queue========\n");
+        printf("<exit>\n");
         while (pTmpNode != NULL) {
             printf("[%d]\n", pTmpNode->data);
             pTmpNode = pTmpNode->nextNode;    
         }
+        printf("<entrance>\n");
         printf("==========================\n");
     }
 }
 
 int main() {
-    Queue* Queue = createCircularQueue();
+    Queue* lQueue = createLinkedListQueue();
 
-    enqueue(Queue, 10);
-    enqueue(Queue, 20);
-    enqueue(Queue, 30);
-    showQueue(Queue);
+    enqueue(lQueue, 10);
+    enqueue(lQueue, 20);
+    enqueue(lQueue, 30);
+    showQueue(lQueue);
 
-    //dequeue(Queue);
-    //dequeue(Queue);
-    dequeue(Queue);
-    dequeue(Queue);
+    dequeue(lQueue);
+    dequeue(lQueue);
+    dequeue(lQueue);
+    dequeue(lQueue);
 
-    showQueue(Queue);
+    showQueue(lQueue);
 
     return 0;
 }
